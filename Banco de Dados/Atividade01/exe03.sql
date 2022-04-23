63e8606a633ffc27ac0fdbcfb1ab8d14cc9ad2e6
Tainã Xavier <102270820+taaixsp@users.noreply.github.com>
2022-04-23 20:34:27 -0300

    Add files via upload

---
+++ Banco de Dados/Atividade01/exe03.sql
@@ -0,0 +1,6 @@
+insert into tb_colaboradores(nome, data_de_nascimento, cpf, endereco, salario) values ("Tainã Xavier", "1992-05-21", 409239388-18, "Av. Manoel Pedro Pimentel, 200", 15000);
+insert into tb_colaboradores(nome, data_de_nascimento, cpf, endereco, salario) values ("Jéssica Silva", "1991-05-21", 303456779-20, "Rua das Maravigolds, 31", 15000);
+insert into tb_colaboradores(nome, data_de_nascimento, cpf, endereco, salario) values ("Mateus Nogueira", "2002-04-06", 232114556-76, "Rua Salete, 20", 30000);
+insert into tb_colaboradores(nome, data_de_nascimento, cpf, endereco, salario) values ("Jefferson Itajahy", "1978-09-22", 556789445-32, "Av. dos Ninjas, 01", 3500);
+insert into tb_colaboradores(nome, data_de_nascimento, cpf, endereco, salario) values ("Vagner Monteiro", "1978-01-09", 889745231-45, "Largo dos Professores, 33", 2500);
+select * from tb_colaboradores
---
+++ Banco de Dados/Atividade01/exe06.sql
@@ -0,0 +1,2 @@
+update tb_colaboradores set salario = 3500 where id = 5;
+select * from tb_colaboradores
